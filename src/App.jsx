7427aad44ba2572824575a6b53c07d6b82fcd3df
Sridhar-Shankar <ssridhar571999@gmail.com>
2025-02-21 22:47:40 +0530

    Add Friends List

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import FriendsList from "./FriendsList";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <FriendsList />
+    </div>
+  );
+}
+export default App;
